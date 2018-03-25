72884bbf76d1cd4466bb142cd4bf43e1c990f220
Federico Elgarte <federicoelgarte@MacBook-Pro-49.local>
2018-03-24 22:31:56 -0300

    test plan

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -23,7 +23,7 @@ const duration = {
   hours:   function(val) { return val * this.minutes(60) },
   days:    function(val) { return val * this.hours(24) },
   weeks:   function(val) { return val * this.days(7) },
-  years:   function(val) { return val * this.days(365) }kmn  
+  years:   function(val) { return val * this.days(365) }
 };
 
 async function liveDeploy(deployer, accounts) {
